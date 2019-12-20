e5323249a212c84c2f66dc372474232eae13b626
William <Willsclvi@gmail.com>
2019-12-20 14:29:05 -0500

    Initial Commit

---
+++ Power Plant Simulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Power Plant Simulator
+//
+//  Created by William Choi on 12/20/19.
+//  Copyright © 2019 William Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
